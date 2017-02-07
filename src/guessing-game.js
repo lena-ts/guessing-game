c83f4cb27c4d0972d20f02d7861c06b9fbd20122
lena <tsybulenko.helen@gmail.com>
2017-02-07 12:09:34 +0300

    Guessing game solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+	   this.min = 0;
+	   this.max = 0;
+	   this.result = 0;
+    }
 
     setRange(min, max) {
-
+		this.min = min;
+		this.max = max;
     }
 
     guess() {
-
+	    this.result = (this.min + this.max)/2;	    
+		return Math.ceil(this.result);
     }
 
     lower() {
-
+	    this.max = Math.ceil(this.result);
     }
 
     greater() {
-
+		this.min = Math.ceil(this.result);
     }
 }
 
-module.exports = GuessingGame;
+module.exports = GuessingGame; 
